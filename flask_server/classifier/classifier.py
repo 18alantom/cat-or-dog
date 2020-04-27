19cf8b2e9c688d98f3f4030263363ae736dbf844
18alantom <2.alan.tom@gmail.com>
2020-04-28 05:25:31 +0530

    try to debug this b....

--- flask_server/classifier/classifier.py
+++ flask_server/classifier/classifier.py
@@ -15,10 +15,12 @@ from .what_is_it import what_is_it
 MEAN = np.array([0.48826352, 0.45509255, 0.4174077])
 STD = np.array([0.22981022, 0.22478424, 0.22537524])
 
-transforms = Compose([
-    ToTensor(),
-    Normalize(MEAN, STD)
-])
+# transforms = Compose([
+# ToTensor(),
+# Normalize(MEAN, STD)
+# ])
+tt = ToTensor()
+no = Normalize(MEAN, STD)
 
 
 def fprint(*args, **kwargs):
@@ -26,8 +28,14 @@ def fprint(*args, **kwargs):
     sys.stdout.flush()
 
 
+# def to_tensor(image):
+#     return transforms(np.uint8(image))
 def to_tensor(image):
-    return transforms(np.uint8(image))
+    image = tt(image)
+    fprint(image.dtype, image.shape, image[0][0])
+    image = no(image)
+    fprint(image.dtype, image.shape, image[0][0])
+    return image
 
 
 def response_to_image(data):
