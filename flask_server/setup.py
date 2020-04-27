2f2c9e10060aea2d78b433b122d453da31d101bf
18alantom <2.alan.tom@gmail.com>
2020-04-28 01:39:21 +0530

    changes

--- flask_server/setup.py
+++ flask_server/setup.py
@@ -4,6 +4,7 @@ setup(
     name='flask_server',
     packages=['flask_server'],
     include_package_data=True,
+    python_requires="3.7.6",
     install_requires=[
         'flask', 'pytorch=1.5.0', 'numpy', 'opencv', 'torchvision'
     ],
